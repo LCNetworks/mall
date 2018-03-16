5e979c78a8ed76b59e6bdaf353ba92d35483677c
lroyzz <244092483@qq.com>
2018-03-16 18:42:04 +0800

    Signed-off-by: lroyzz <244092483@qq.com>购物车

--- client/pages/goodsDetail/goodsDetail.js
+++ client/pages/goodsDetail/goodsDetail.js
@@ -44,6 +44,47 @@ Page({
       }
     ],
   },
+  //购物车
+  showModal: function () {
+    // 显示遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      shopCart: animation.export(),
+      showModalStatus: true
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        shopCart: animation.export()
+      })
+    }.bind(this), 200)
+  },
+  hideModal: function () {
+    // 隐藏遮罩层
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      shopCart: animation.export(),
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        shopCart: animation.export(),
+        showModalStatus: false
+      })
+    }.bind(this), 200)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
